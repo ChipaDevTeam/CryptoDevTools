1c6b2939bc071d8639fb9549e829163742405461
Your Name <you@example.com>
2025-09-16 15:20:16 -0300

    Add getGraduatedTokens method to SolanaDataClient for retrieving graduated tokens

--- CryptoDevTools/solana/_DataClient.py
+++ CryptoDevTools/solana/_DataClient.py
@@ -11,3 +11,7 @@ class SolanaDataClient:
         if exchange_name not in GlobalConstants.EXCHANGES:
             raise ValueError(f"Exchange '{exchange_name}' is not supported.")
         return self.token_api.get_new_tokens(exchange_name=exchange_name)
+    def getGraduatedTokens(self, sortBy=GlobalConstants.GRADUATED_DEFAULT_SORT):
+        if sortBy not in GlobalConstants.SORT_BY_OPTIONS:
+            raise ValueError(f"Invalid sortBy value. Must be one of {GlobalConstants.SORT_BY_OPTIONS}")
+        return self.token_api.get_graduated_tokens(sortBy=sortBy)
