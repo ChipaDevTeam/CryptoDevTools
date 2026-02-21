18ec3630346c5becbd153a80dcff877ffb69470f
Vigo Walker <vigopaul05@gmail.com>
2026-02-21 13:03:19 +0100

    Update TOKEN_MINT address in listenToSwaps.py for monitoring

--- examples/Solana/listenToSwaps.py
+++ examples/Solana/listenToSwaps.py
@@ -16,7 +16,7 @@ async def main():
     
     # Replace with the Token Mint Address you want to monitor
     # Example: POPCAT
-    TOKEN_MINT = "2vaM39Bbbxxbt2WC3JcPxbPDvJ6WncHBrH1GqtXV3QSC" 
+    TOKEN_MINT = "6sUrXQq46D9VeMpb2tJAEVyw5QEGCCZ2gRftq49TdMJy" 
 
     print(f"Initializing listener for {TOKEN_MINT}...")
     listener = SolanaSwapListener(RPC_URL, WSS_URL)
